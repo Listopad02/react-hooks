d60818da5ef5d949b020f8a7802969e580e02071
Listopad02 <123ayetvbass@gmail.com>
2021-12-26 22:32:05 +0300

    useEffect hook reviewed

--- src/App.js
+++ src/App.js
@@ -1,52 +1,10 @@
-import { useState } from "react";
-
-function computeInitialCounter() {
-    console.log('Some calculations...');
-    return Math.trunc(Math.random() * 20);
-}
+import { useState, useEffect } from "react";
 
 function App() {
-    // const [counter, setCounter] = useState(0)
-    // const [counter, setCounter] = useState(computeInitialCounter())
-    const [counter, setCounter] = useState(() => {
-        return computeInitialCounter()
-    }) 
-
-    const [state, setState] = useState({
-        title: 'counter',
-        date: Date.now()
-    })
-
-    function increment() {
-        // setCounter(counter + 1)
-        // setCounter(counter + 1)
-        setCounter((prevCounter) => {
-            return prevCounter + 1
-        })
-        //setCounter(prev => prev + 1)
-    }
-
-    function decrement() {
-        setCounter(counter - 1)
-    }
-
-    function updateTitle() {
-        setState(prev => {
-            return {
-                ...prev,
-                title: 'New title'
-            }
-        })
-    }
 
     return (
         <div>
-            <h1>Counter: {counter}</h1>
-            <button onClick={increment}>Add</button>
-            <button onClick={decrement}>Sub</button>
-            <button onClick={updateTitle}>Change title</button>
-
-            <pre>{JSON.stringify(state, null, 2)}</pre>
+            
         </div>
     )
 }
